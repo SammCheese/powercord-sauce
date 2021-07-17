91d69ea0ae65729329ab3cee5130dd55cfda5d8f
SammCheese <sammcheese.github@gmail.com>
2021-07-18 00:19:27 +0200

    Fixing Autocomplete not showing

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ module.exports = class TheSauce extends Plugin {
       description: "Look up your Sauce in discord!",
       usage: "[prefix]sauce < Sauce Code | Sauce Link | Random >",
       executor: async ([args]) => {
-        if (!args) {
-          args = await api.randomDoujinID(); //Failsafe
-        }
-
         if (args.startsWith("https://nhentai.net")) {
           args = args.match(SauceRegex)[0].replace("/g/", "");
         }
@@ -29,14 +25,14 @@ module.exports = class TheSauce extends Plugin {
 
         return chatembed.executor(args);
       },
-      autocomplete: ([args]) => {
+      autocomplete: (args) => {
         if (args.length !== 1) {
           return false;
         }
         let options = {
+          random: "Get a Random Doujin from nhentai.net",
           Sauce:
             "Get Data from Code or link. i.e 52672 or https://nhentai.net/g/52672/",
-          random: "Get a Random Doujin from nhentai.net",
         };
         return {
           commands: Object.keys(options)
