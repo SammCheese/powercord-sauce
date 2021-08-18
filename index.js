e1fb523e90f5484b14b5952d2b0a693acddcb5e5
Samm Cheese <sammcheese.github@gmail.com>
2021-08-18 05:05:26 +0200

    Autocomplete sucks, removing.

--- index.js
+++ index.js
@@ -14,33 +14,13 @@ module.exports = class TheSauce extends Plugin {
       description: 'Look up your Sauce in discord!',
       usage: '[prefix]sauce < Sauce Code | Sauce Link | Random >',
       executor: async (args) => {
-        if (args[1] && args[1].startsWith('https://nhentai.net')) {
-          args[1] = args[1].match(SauceRegex)[0].replace('/g/', '');
+        if (args[0] && args[0].startsWith('https://nhentai.net')) {
+          args[0] = args[0].match(SauceRegex)[0].replace('/g/', '');
         }
-        if (args[0] === 'Random'.toLowerCase() || !args[1]) {
-          args[1] = await api.randomDoujinID();
+        if (args[0] === 'Random'.toLowerCase() || !args[0]) {
+          args[0] = await api.randomDoujinID();
         }
-
-        return chatembed.executor(args[1]);
-      },
-      autocomplete: (args) => {
-        if (args.length !== 1) {
-          return false;
-        }
-        const options = {
-          random: 'Get a Random Doujin from nhentai.net',
-          Sauce:
-            'Get Data from Code or link. i.e 52672 or https://nhentai.net/g/52672/'
-        };
-        return {
-          commands: Object.keys(options)
-            .filter((option) => option.includes(args[0].toLowerCase()))
-            .map((option) => ({
-              command: option,
-              description: options[option]
-            })),
-          header: 'Sauce Commands'
-        };
+        return chatembed.executor(args[0]);
       }
     });
   }
